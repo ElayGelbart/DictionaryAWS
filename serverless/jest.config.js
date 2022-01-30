d3748b7f4447328b51ef0587e65571257b97a62d
ElayGelbart <elaygelbart@gmail.com>
2022-01-30 13:05:57 +0200

    new tests server

--- serverless/jest.config.js
+++ serverless/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/test', '<rootDir>/back/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
